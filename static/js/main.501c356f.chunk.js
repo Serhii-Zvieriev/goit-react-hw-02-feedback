8ab0f19d6291743d41c1ac8fd875629d19015624
Serhii-Zvieriev <zacjok@gmail.com>
2021-09-27 22:02:17 +0300

    Updates

--- static/js/main.501c356f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),a=t.n(s),i=(t(8),t(9),t(0));var p=function(){return Object(i.jsx)("div",{className:"App",children:"feedback"})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(p,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.501c356f.chunk.js.map
