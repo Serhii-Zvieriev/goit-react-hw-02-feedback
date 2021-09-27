5aad8e92bdd768761ae2616a509a45708a3f6dee
Serhii-Zvieriev <zacjok@gmail.com>
2021-09-27 22:01:44 +0300

    step 2 complite

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Feedback from './components/Feedback';
 
 function App() {
-  return <div className="App">feedback</div>;
+  return (
+    <div className="App">
+      <Feedback />
+    </div>
+  );
 }
 
 export default App;
